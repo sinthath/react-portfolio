cb05f9107cf4fdcb89f244b75b377b2b6b88e67a
sinthath <sinthathirath@gmail.com>
2022-04-23 01:20:41 -0400

    nav listed

---
+++ src/components/Nav/index.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function Nav(props) {
+
+  const {
+    pages = [],
+    setCurrentPage,
+    currentPage
+  } = props;
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentPage.name);
+  }, [currentPage]);
+
+  return (
+    <nav>
+      <ul className="flex-row">
+        {pages.map((page) => (
+          <li className={`mx-1 ${currentPage.name === page.name && 'navActive'}`}
+          key={page.name}
+          >
+          <span onClick={() => setCurrentPage(page)}>
+            {capitalizeFirstLetter(page.name)}  
+          </span>  
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav;s
