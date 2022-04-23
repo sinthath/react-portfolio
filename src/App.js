5ca275ecd98fe5fcfbcf63b31f0960439135d330
sinthath <sinthathirath@gmail.com>
2022-04-23 00:54:12 -0400

    nav render

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Nav from './components/Nav';
+//import About from './components/About';
+//import Contact from './components/Contact';
+//import Resume from './components/Resume';
+//import Projects from './components/Projects';
+import Links from './components/Link'
 
 function App() {
+  const [pages] = useState([
+    {name: 'about me' },
+    {name: 'projects'},
+    {name: 'resume'},
+    {name: 'contact'}
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <Nav
+        pages={pages}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+        ></Nav>
+      </Header>
+      <main>
+        <Links currentPage={currentPage}></Links>
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
